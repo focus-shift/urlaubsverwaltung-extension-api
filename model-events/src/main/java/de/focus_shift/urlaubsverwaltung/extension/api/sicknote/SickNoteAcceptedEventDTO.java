79e52b0751e59c7148319e1cead97a2c52dfe149
Johannes Graf <graf.johannes@gmail.com>
2025-02-18 22:38:16 +0100

    introduce SickNoteAcceptedEventDTO

---
+++ model-events/src/main/java/de/focus_shift/urlaubsverwaltung/extension/api/sicknote/SickNoteAcceptedEventDTO.java
@@ -0,0 +1,37 @@
+package de.focus_shift.urlaubsverwaltung.extension.api.sicknote;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+@Value
+@Builder
+@Jacksonized
+public class SickNoteAcceptedEventDTO {
+  @NonNull private UUID id;
+
+  @NonNull private Long sourceId;
+
+  @NonNull private Instant createdAt;
+
+  @NonNull private String tenantId;
+
+  @NonNull private SickNotePersonDTO person;
+
+  private SickNotePersonDTO applier;
+
+  @NonNull private String type;
+  @NonNull private String status;
+
+  @NonNull private SickNotePeriodDTO period;
+
+  private SickNotePeriodDTO medicalCertificatePeriod;
+
+  @NonNull private Set<LocalDate> absentWorkingDays;
+}
