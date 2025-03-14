e35d633da1229ec7ae5ee6f7a9e7020d01d036bf
honnel <github@honnel.de>
2025-03-14 13:37:27 +0100

    Add ApplicationUpdatedEventDto model

---
+++ model-events/src/main/java/de/focus_shift/urlaubsverwaltung/extension/api/application/ApplicationUpdatedEventDTO.java
@@ -0,0 +1,44 @@
+package de.focus_shift.urlaubsverwaltung.extension.api.application;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+@Value
+@Builder
+@Jacksonized
+public class ApplicationUpdatedEventDTO {
+  @NonNull private UUID id;
+
+  @NonNull private Long sourceId;
+
+  @NonNull private Instant createdAt;
+
+  @NonNull private String tenantId;
+
+  @NonNull private ApplicationPersonDTO person;
+
+  private ApplicationPersonDTO appliedBy;
+
+  private ApplicationPersonDTO updatedBy;
+
+  private boolean twoStageApproval;
+
+  @NonNull private ApplicationPeriodDTO period;
+
+  @NonNull private VacationTypeDTO vacationType;
+
+  private String reason;
+
+  @NonNull private String status;
+
+  private boolean teamInformed;
+
+  @NonNull private Set<LocalDate> absentWorkingDays;
+}
