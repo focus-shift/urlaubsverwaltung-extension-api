53713462be6b515fd8b1796bd9026896c9088e58
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-11-25 14:37:18 +0100

    Fix PersonDTO enabled default value in builder

--- model-common/src/main/java/de/focus_shift/urlaubsverwaltung/extension/api/person/PersonDTO.java
+++ model-common/src/main/java/de/focus_shift/urlaubsverwaltung/extension/api/person/PersonDTO.java
@@ -7,17 +7,37 @@ import java.util.Set;
 
 import static de.focus_shift.urlaubsverwaltung.extension.api.person.RoleDTO.USER;
 
-
 public record PersonDTO(
         Long id,
         @NonNull String username,
         @NonNull String lastName,
         @NonNull String firstName,
         @NonNull String email,
-        boolean enabled,
+        Boolean enabled,
         @NonNull Set<RoleDTO> permissions,
         @NonNull Set<MailNotificationDTO> notifications
 ) {
+
+    @Builder
+    public PersonDTO(
+            Long id,
+            @NonNull String username,
+            @NonNull String lastName,
+            @NonNull String firstName,
+            @NonNull String email,
+            Boolean enabled,
+            @NonNull Set<RoleDTO> permissions,
+            @NonNull Set<MailNotificationDTO> notifications
+    ) {
+        this.id = id;
+        this.username = username;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+        this.enabled = enabled != null ? enabled : Boolean.TRUE;
+        this.permissions = permissions;
+        this.notifications = notifications;
+    }
 
     @Builder
     public PersonDTO(
--- model-common/src/test/java/de/focus_shift/urlaubsverwaltung/extension/api/person/PersonDTOTest.java
+++ model-common/src/test/java/de/focus_shift/urlaubsverwaltung/extension/api/person/PersonDTOTest.java
@@ -1,64 +1,91 @@
 package de.focus_shift.urlaubsverwaltung.extension.api.person;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Set;
-import org.junit.jupiter.api.Test;
-
 class PersonDTOTest {
 
-  @Test
-  void happyPath() {
+    @Test
+    void happyPath() {
 
-    final PersonDTO person =
-        PersonDTO.builder()
-            .username("61f886fd-e07c-4cc3-add0-d869520172e1")
-            .lastName("Muster")
-            .firstName("Marlene")
-            .email("marlene.muster@example.org")
-            .permissions(Set.of(RoleDTO.USER))
-            .notifications(Set.of(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED))
-            .build();
+        final PersonDTO person =
+                PersonDTO.builder()
+                        .username("61f886fd-e07c-4cc3-add0-d869520172e1")
+                        .lastName("Muster")
+                        .firstName("Marlene")
+                        .email("marlene.muster@example.org")
+                        .permissions(Set.of(RoleDTO.USER))
+                        .notifications(Set.of(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED))
+                        .build();
 
-    assertThat(person).isNotNull();
-    assertThat(person.id()).isNull();
-    assertThat(person.username()).isEqualTo("61f886fd-e07c-4cc3-add0-d869520172e1");
-    assertThat(person.lastName()).isEqualTo("Muster");
-    assertThat(person.firstName()).isEqualTo("Marlene");
-    assertThat(person.email()).isEqualTo("marlene.muster@example.org");
-    assertThat(person.enabled()).isTrue();
-    assertThat(person.permissions()).containsOnly(RoleDTO.USER);
-    assertThat(person.notifications())
-        .containsOnly(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED);
-  }
+        assertThat(person).isNotNull();
+        assertThat(person.id()).isNull();
+        assertThat(person.username()).isEqualTo("61f886fd-e07c-4cc3-add0-d869520172e1");
+        assertThat(person.lastName()).isEqualTo("Muster");
+        assertThat(person.firstName()).isEqualTo("Marlene");
+        assertThat(person.email()).isEqualTo("marlene.muster@example.org");
+        assertThat(person.enabled()).isTrue();
+        assertThat(person.permissions()).containsOnly(RoleDTO.USER);
+        assertThat(person.notifications())
+                .containsOnly(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED);
+    }
 
-  @Test
-  void canBeEnabled() {
-    assertThat(anyPerson().enable().enabled()).isTrue();
-  }
+    @Test
+    void ensureThatYouCanSetEnabledWithTheBuilder() {
 
-  @Test
-  void canBeDisabled() {
-    assertThat(anyPerson().disable().enabled()).isFalse();
-  }
+        final PersonDTO person =
+                PersonDTO.builder()
+                        .username("61f886fd-e07c-4cc3-add0-d869520172e1")
+                        .lastName("Muster")
+                        .firstName("Marlene")
+                        .email("marlene.muster@example.org")
+                        .enabled(false)
+                        .permissions(Set.of(RoleDTO.USER))
+                        .notifications(Set.of(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED))
+                        .build();
 
-  @Test
-  void handlesMissingParams() {
+        assertThat(person).isNotNull();
+        assertThat(person.id()).isNull();
+        assertThat(person.username()).isEqualTo("61f886fd-e07c-4cc3-add0-d869520172e1");
+        assertThat(person.lastName()).isEqualTo("Muster");
+        assertThat(person.firstName()).isEqualTo("Marlene");
+        assertThat(person.email()).isEqualTo("marlene.muster@example.org");
+        assertThat(person.enabled()).isFalse();
+        assertThat(person.permissions()).containsOnly(RoleDTO.USER);
+        assertThat(person.notifications())
+                .containsOnly(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED);
+    }
 
-    assertThatThrownBy(() -> PersonDTO.builder().build())
-        .isInstanceOf(NullPointerException.class)
-        .hasMessageStartingWith("username is marked non-null but is null");
-  }
+    @Test
+    void canBeEnabled() {
+        assertThat(anyPerson().enable().enabled()).isTrue();
+    }
 
-  private static PersonDTO anyPerson() {
-    return PersonDTO.builder()
-        .username("61f886fd-e07c-4cc3-add0-d869520172e1")
-        .lastName("Muster")
-        .firstName("Marlene")
-        .email("marlene.muster@example.org")
-        .permissions(Set.of(RoleDTO.USER))
-        .notifications(Set.of(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED))
-        .build();
-  }
+    @Test
+    void canBeDisabled() {
+        assertThat(anyPerson().disable().enabled()).isFalse();
+    }
+
+    @Test
+    void handlesMissingParams() {
+
+        assertThatThrownBy(() -> PersonDTO.builder().build())
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageStartingWith("username is marked non-null but is null");
+    }
+
+    private static PersonDTO anyPerson() {
+        return PersonDTO.builder()
+                .username("61f886fd-e07c-4cc3-add0-d869520172e1")
+                .lastName("Muster")
+                .firstName("Marlene")
+                .email("marlene.muster@example.org")
+                .permissions(Set.of(RoleDTO.USER))
+                .notifications(Set.of(MailNotificationDTO.NOTIFICATION_EMAIL_APPLICATION_APPLIED))
+                .build();
+    }
 }
