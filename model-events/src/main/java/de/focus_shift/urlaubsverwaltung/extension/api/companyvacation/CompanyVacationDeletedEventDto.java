b1642e15603d45d28beb05581f30c14896c860b9
honnel <github@honnel.de>
2025-12-03 23:31:49 +0100

    Add CompanyVacationDeleted Event

---
+++ model-events/src/main/java/de/focus_shift/urlaubsverwaltung/extension/api/companyvacation/CompanyVacationDeletedEventDto.java
@@ -0,0 +1,15 @@
+package de.focus_shift.urlaubsverwaltung.extension.api.companyvacation;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Builder
+public record CompanyVacationDeletedEventDto(
+        @NonNull UUID id,
+        @NonNull String sourceId,
+        @NonNull Instant deletedAt,
+        @NonNull String tenantId) {
+}
